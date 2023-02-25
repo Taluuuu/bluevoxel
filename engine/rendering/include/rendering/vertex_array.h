75afbfc96cd213d051e63baf5b03a52f39f07532
Charles-Antoine Lord <charlesantlord@gmail.com>
2023-02-25 14:28:25 -0500

    Added vertex array interface missing in vcs

---
+++ engine/rendering/include/rendering/vertex_array.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "core/types.h"
+
+#include <memory>
+
+namespace engine
+{
+    class IBuffer;
+
+    class IVertexArray
+    {
+    public:
+
+        virtual ~IVertexArray() = default;
+
+        virtual void attach_vertex_buffer(const std::shared_ptr<IBuffer>& buffer, u32 binding_index, i64 offset, i32 stride) = 0;
+        virtual void setup_attribute(u32 attribute_index, u32 binding_index, i32 size, u32 relative_offset) = 0;
+
+    };
+}
